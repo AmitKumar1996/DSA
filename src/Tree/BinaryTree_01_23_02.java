63bf3dd712f85113e23acf70c91088bab3447773
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-23 17:09:03 +0530

    DSA_01_23_02

--- src/Tree/BinaryTree_01_23_02.java
+++ src/Tree/BinaryTree_01_23_02.java
@@ -1,49 +1,57 @@
 package Tree;
 
-public class BinaryTree_01_23 {
-
+public class BinaryTree_01_23_02 {
+	
 	static class Node{
+		
 		int data;
+		
 		Node left;
 		Node right;
 		
 		Node(int data){
-			this.data=data;
-			this.left=null;
+			
+			this.data= data;
 			this.right=null;
+			this.left=null;
 		}
+		
+		
 	}
 	
-	static class BinaryTree {
-		
+	static class BinaryTree{
 		static int idx=-1;
 		
 		public static Node buildTree(int nodes[]) {
+			
 			idx++;
 			
-			if(idx>= nodes.length ||   nodes[idx] == -1) {
+			if(idx>= nodes.length || nodes[idx]== -1) {
 				return null;
 			}
+			Node newNode= new Node(nodes[idx]);
 			
-			Node newNode = new Node(nodes[idx]);
-			
-			newNode.left = buildTree(nodes);
+			newNode.left=buildTree(nodes);
 			
 			newNode.right=buildTree(nodes);
 			return newNode;
+			
+			
 		}
 		
+		
 	}
-	// kumar amit
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int nodes[]= {1,2,3,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		
 		BinaryTree tree= new BinaryTree();
 		
-		Node root= tree.buildTree(nodes);
+		Node root=tree.buildTree(nodes);
 		
 		System.out.println(root.data);
+				
+		// TODO Auto-generated method stub
 
 	}
 
