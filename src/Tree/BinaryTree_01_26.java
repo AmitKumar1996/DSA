d826633d88ae93aa1649e297f18d9f13fd617c76
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-26 02:17:28 +0530

    BinarryTree_01_26

--- src/Tree/BinaryTree_01_26.java
+++ src/Tree/BinaryTree_01_26.java
@@ -2,7 +2,7 @@ package Tree;
 
 public class BinaryTree_01_26 {
 	
-	class Node{
+static	class Node{
 		
 		int data;
 		Node left;
@@ -17,7 +17,23 @@ public class BinaryTree_01_26 {
 	
      static class BinaryTree{
     	 
+    static int idx=-1;
     	 
+    	 public static Node buildTree(int nodes[]) {
+    		 idx++;
+    		 
+    		 if(idx>= nodes.length || nodes[idx] == -1) {
+    			 return null;
+    		 }
+    		 
+    		 Node newNode = new Node(nodes[idx]);
+    		 
+    		 newNode.left= buildTree(nodes);
+    		 newNode.right= buildTree(nodes);
+    		 
+    		 return newNode;
+    		 
+    	 }
      }
 	
 
