19ab0802143340a811ae13438a62c09ccbe21b31
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-26 02:01:44 +0530

    BinarryTree_01_26

---
+++ src/Tree/BinaryTree_01_26.java
@@ -0,0 +1,29 @@
+package Tree;
+
+public class BinaryTree_01_26 {
+	
+	class Node{
+		
+		int data;
+		Node left;
+		Node right;
+		
+	     Node(int data){
+	    	 this.data= data;
+	    	 this.left=null;
+	    	 this.right=null;
+	     }
+	}
+	
+     static class BinaryTree{
+    	 
+    	 
+     }
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
