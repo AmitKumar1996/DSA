0bbbda918daa620442d22ec559d133849190f498
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-27 19:29:01 +0530

    BinarryTree_01_27

---
+++ src/Tree/BinaryTree_01_27.java
@@ -0,0 +1,59 @@
+package Tree;
+
+import Tree.BinarryTree_01_24.BinaryTree;
+
+public class BinaryTree_01_27 {
+	
+static class Node{
+	
+	int data;
+	Node left;
+	Node right;
+	
+	Node(int data){
+		this.data=data;
+		this.left=null;
+		this.right=null;
+	}
+	
+private static	class BinaryTree{
+		
+		static int idx=-1;
+		
+		public static Node buildTree(int nodes[]) {
+			
+			idx++;
+			
+			if(idx>= nodes.length || nodes[idx]== -1) {
+				return null;
+			}
+			
+			Node newNode = new Node(nodes[idx]);
+			
+			newNode.left=buildTree(nodes);
+			newNode.right= buildTree(nodes);
+			
+			return newNode;
+		
+			
+			
+			
+		}
+	}
+}
+
+	public static void main(String[] args) {
+		
+		int nodes[]= {1,2,3,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		
+		BinaryTree tree= new BinaryTree();
+		
+		Tree.BinarryTree_01_24.Node root = tree.buildTree(nodes);
+		
+		System.out.println(root.data);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
