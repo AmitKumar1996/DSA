3dd2c5559f8708560ebd220dbf40d91d7b39ff3e
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-28 10:44:57 +0530

    BinarryTree_01_28

---
+++ src/Tree/BinaryTree_01_28.java
@@ -0,0 +1,45 @@
+package Tree;
+
+public class BinaryTree_01_28 {
+	
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data){
+			this.data=data;
+			this.left=null;
+			left.right=null;
+		}
+		
+	static	class BinaryTree{
+		
+		static int idx=-1;
+		public static Node buildTree(int nodes[]) {
+			
+			idx++;
+			if(idx>= nodes.length || nodes[idx] == -1) {
+				return null;
+				
+			}
+			Node newNode= new Node(nodes[idx]);
+			
+			newNode.left=buildTree(nodes);
+			newNode.right=buildTree(nodes);
+			
+			return newNode;
+			
+			
+			
+		}
+	}
+	}
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
