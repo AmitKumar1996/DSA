48b1a02af7e9417b21023e2f028b8ad5b1e113a6
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-05 20:52:28 +0530

    Quick Sort_02_05

---
+++ src/Tree/BinaryTree_02_05.java
@@ -0,0 +1,31 @@
+package Tree;
+
+public class BinaryTree_02_05 {
+	
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data){
+			this.data=data;
+			this.left=null;
+			this.right=null;
+		}
+		
+		
+	}
+	
+	static class BinaryTree{
+		
+		static int idx=-1;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
