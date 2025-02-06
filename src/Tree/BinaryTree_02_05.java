2109297da564462a9ac83dfacbb8f78083855f9a
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-06 16:52:16 +0530

    Quick Sort_02_05

--- src/Tree/BinaryTree_02_05.java
+++ src/Tree/BinaryTree_02_05.java
@@ -21,6 +21,7 @@ public class BinaryTree_02_05 {
 		static int idx=-1;
 		
 		public Node buildTree(int[] nodes) {
+			idx++;
 			if(idx>=nodes.length || nodes[idx]== -1) {
 				return null;
 			}
@@ -34,6 +35,13 @@ public class BinaryTree_02_05 {
 	}
 
 	public static void main(String[] args) {
+		
+		int nodes[]= {1,2,3,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		
+		BinaryTree tree= new BinaryTree();
+		Node root=tree.buildTree(nodes);
+		
+		System.out.println(root.data);
 		// TODO Auto-generated method stub
 
 	}
